381497941096840f52249bcce4ce5e0ed93f7b5e
liz-fw <lstebbins@flowwest.com>
2026-03-09 14:47:44 -0700

    cleans up param references for later date

--- R/surv_egg_to_fry.R
+++ R/surv_egg_to_fry.R
@@ -4,8 +4,6 @@
 #' @details See \code{\link{params}} for details on parameter sources
 #' @param proportion_natural Variable describing the proportion of natural-origin spawners
 #' @param scour Variable describing the probability of redd scouring event
-#' @param temperature_effect Variable describing the effect of inchannel temperature on egg survival.
-#' Fall and spring estimated by C. Hammersmark (CBEC Ecoengineering Inc.). Winter-run value was calibrated.
 #' @param .surv_egg_to_fry_int Intercept
 #' @param .proportion_natural Coefficient for \code{proportion_natural} variable
 #' @param ..surv_egg_to_fry_mean_egg_temp_effect TODO
