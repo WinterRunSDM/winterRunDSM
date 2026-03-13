d760f53c32015de3b92ae1074ab78a351ea98d2f
Catarina Pien <catarina.pien@gmail.com>
2026-03-13 11:40:53 -0700

    Added parameter for kwk survival with volitional passage

--- wr_sdm/parameter_lists/wr_sdm_baseline_parameters.R
+++ wr_sdm/parameter_lists/wr_sdm_baseline_parameters.R
@@ -192,7 +192,13 @@ wr_sdm_baseline_params <- list(
   
   # winter Run SDM inputs
   adult_enroute_surv_mult = setNames(c(1, rep(1, 30)), winterRunDSM::watershed_labels),
-  incidental_trib_harvest = setNames(rep(0.1, 31), winterRunDSM::watershed_labels)
+  incidental_trib_harvest = setNames(rep(0.1, 31), winterRunDSM::watershed_labels),
+  # survival estimate for adults - volitional passage past Keswick Dam
+  effect_upstream_vol_adult_kwk = 0,
+  # survival estimate for juveniles - volitional passage past Keswick Dam
+  effect_upstream_vol_juv_kwk = 0
+  
+  
   
 )
 
